e33a2e53919318cdd49582c61e5f83d437d2e185
mfatekho <marat.fatekhov@intel.com>
2021-05-03 17:41:34 +0300

    Functional layout

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,68 @@
-import { Component } from '@angular/core';
+import {Component} from '@angular/core';
+import {AbstractControl, FormBuilder, FormGroup, Validators} from '@angular/forms';
+
+interface RadioValue {
+  label: string;
+  value: boolean | string;
+}
+
+interface ConstructorField {
+  label: string;
+  values: RadioValue[];
+  disabled?: boolean;
+  default?: boolean | string;
+}
+
+const openVINOInstalledField: ConstructorField = {
+  label: 'I have OpenVINO installed',
+  values: [{label: 'Yes', value: true}, {label: 'No', value: false}],
+  default: true
+};
+
+const dockerInstalledField: ConstructorField = {
+  label: 'I have Docker installed',
+  values: [{label: 'Yes', value: true}, {label: 'No', value: false}],
+  default: true
+};
+
+const machineOsField: ConstructorField = {
+  label: 'My OS',
+  values: [{label: 'Linux', value: 'Linux'}, {label: 'Windows', value: 'Windows'}, {label: 'Ubuntu', value: 'Ubuntu'}],
+  default: 'Linux'
+};
+
+const availableDevices: ConstructorField = {
+  label: 'Accelerators on my machine',
+  values: [{label: 'CPU', value: 'CPU'}, {label: 'GPU', value: 'GPU'}, {label: 'NCS2', value: 'NCS2'}, {
+    label: 'HDDL',
+    value: 'HDDL'
+  }],
+  default: 'CPU'
+};
+
+const startWithField: ConstructorField = {
+  label: 'Start DL Workbench with',
+  values: [{label: 'Python wrapper', value: 'pip'}, {label: 'Plain Docker Command', value: 'docker'}],
+  default: 'pip'
+};
+
+const useHttpProxy: ConstructorField = {
+  label: 'Use HTTP Proxy',
+  values: [{label: 'Yes', value: true}, {label: 'No', value: false}],
+  default: false
+};
+
+const useHttpsProxy: ConstructorField = {
+  label: 'Use HTTPS Proxy',
+  values: [{label: 'Yes', value: true}, {label: 'No', value: false}],
+  default: false
+};
+
+const useNoProxy: ConstructorField = {
+  label: 'Use No Proxy',
+  values: [{label: 'Yes', value: true}, {label: 'No', value: false}],
+  default: false
+};
 
 @Component({
   selector: 'app-root',
@@ -7,4 +71,60 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'wb-launch-constructor';
+  public launchConstructorForm: FormGroup;
+  public openVINOInstalledField = openVINOInstalledField;
+  public dockerInstalledField = dockerInstalledField;
+  public machineOsField = machineOsField;
+  public availableDevices = availableDevices;
+  public startWithField = startWithField;
+  public useHttpProxy = useHttpProxy;
+  public useHttpsProxy = useHttpsProxy;
+  public useNoProxy = useNoProxy;
+
+  constructor(private formBuilder: FormBuilder) {
+    this.launchConstructorForm = this.formBuilder.group({
+      openVINOInstalled: [this.openVINOInstalledField.default, Validators.required],
+      dockerInstalled: [this.dockerInstalledField.default, Validators.required],
+      machineOS: [this.machineOsField.default, Validators.required],
+      devices: [[this.availableDevices.default], Validators.required],
+      wbLauncher: [this.startWithField.default, Validators.required],
+      httpProxy: [this.useHttpProxy.default, Validators.required],
+      httpsProxy: [this.useHttpsProxy.default, Validators.required],
+      noProxy: [this.useNoProxy.default, Validators.required],
+    });
+  }
+
+  isOptionSelected(controlName: string, optionValue: boolean | string): boolean {
+    return this.launchConstructorForm.get(controlName)?.value === optionValue;
+  }
+
+  isDeviceSelected(optionValue: boolean | string): boolean {
+    return this.launchConstructorForm.get('devices')?.value.includes(optionValue);
+  }
+
+  deviceSelected(optionValue: boolean | string): void {
+    const selectedDevices = this.launchConstructorForm.get('devices')?.value;
+    if (selectedDevices.includes(optionValue)) {
+      const index = selectedDevices.indexOf(optionValue, 0);
+      if (index > -1) {
+        selectedDevices.splice(index, 1);
+      }
+    } else {
+      selectedDevices.push(optionValue);
+    }
+    this.launchConstructorForm.get('devices')?.patchValue(selectedDevices);
+
+  }
+
+  get useHttpProxyControl(): AbstractControl | null {
+    return this.launchConstructorForm.get('httpProxy');
+  }
+
+  get useHttpsProxyControl(): AbstractControl | null {
+    return this.launchConstructorForm.get('httpsProxy');
+  }
+
+  get useNoProxyControl(): AbstractControl | null {
+    return this.launchConstructorForm.get('noProxy');
+  }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,33 @@
-import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
+import {BrowserModule} from '@angular/platform-browser';
+import {NgModule} from '@angular/core';
 
-import { AppComponent } from './app.component';
+import {AppComponent} from './app.component';
+import {ReactiveFormsModule} from '@angular/forms';
+import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
+import {MatCheckboxModule} from '@angular/material/checkbox';
+import {MatRadioModule} from '@angular/material/radio';
+import {MatFormFieldModule} from '@angular/material/form-field';
+import {MatInputModule} from '@angular/material/input';
+import {MatButtonModule} from '@angular/material/button';
+import {MatIconModule} from '@angular/material/icon';
 
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    ReactiveFormsModule,
+    BrowserAnimationsModule,
+    MatCheckboxModule,
+    MatRadioModule,
+    MatFormFieldModule,
+    MatInputModule,
+    MatButtonModule,
+    MatIconModule
   ],
   providers: [],
   bootstrap: [AppComponent]
 })
-export class AppModule { }
+export class AppModule {
+}
